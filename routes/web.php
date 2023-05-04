859c7dcb007728058320cde38e943921fc8df814
Tayyabshahzad <tayyabshahzad1@outlook.com>
2023-05-05 00:47:51 +0500

    new middleware and auth

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes // new
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
