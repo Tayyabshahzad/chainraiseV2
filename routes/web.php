967b8676611c3b931fe7fca3d2cf17badaffeff7
Tayyabshahzad <tayyabshahzad1@outlook.com>
2023-05-05 01:06:56 +0500

    issue gijjt

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 /*
 |--------------------------------------------------------------------------
-| Web Routes // new // eew tayyab
+| Web Routes // new // eew tayyab hamza
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
