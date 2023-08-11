7e4ed119e537c1a3833ae13621e5cba199acda2d
Tayyabshahzad <tayyabshahzad1@outlook.com>
2023-08-11 22:25:50 +0500

    E Signature

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -34,44 +34,7 @@ class WebhookController extends Controller
 
     public function esignatures(Request $request)
     {
-        
-        try {
-            // Get the raw content of the request
-            $rawContent = $request->getContent(); 
-            // Find the position where the JSON data starts
-            $jsonStartPos = strrpos($rawContent, '{'); 
-            // Extract the JSON data
-            $jsonData = substr($rawContent, $jsonStartPos);
-
-            // Parse the JSON data
-            $data = json_decode($jsonData, true);
-
-            // Log the parsed JSON data
-            Log::info('Webhook JSON Data:', $data);
-
-            // Extract relevant information
-            if (isset($data['status'])) {
-                $status = $data['status'];
-            } else {
-                Log::error("'status' key is missing in JSON data");
-                // Handle the error gracefully
-            }
-
-            if (isset($data['data']['contract']['id'])) {
-                $contractId = $data['data']['contract']['id'];
-            } else {
-                Log::error("'contract' or 'id' key is missing in JSON data");
-                // Handle the error gracefully
-            }
-
-            // ... Process other relevant data ...
-
-            // Return a response
-           // return response()->json(['message' => 'Webhook received successfully']);
-        } catch (\Exception $e) {
-            Log::error('Error processing webhook:', ['message' => $e->getMessage()]);
-           // return response()->json(['message' => 'Error processing webhook'], 500);
-        }
+        dd($request);
     }
      
  
