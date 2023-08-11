e23080ed2c4e1929a419bc675e12f7f98b04c78f
Tayyabshahzad <tayyabshahzad1@outlook.com>
2023-08-11 22:40:04 +0500

    E Signature

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -33,27 +33,37 @@ class WebhookController extends Controller
     }
 
     public function esignatures(Request $request)
-{
-    try {
-        // Get the raw content of the request
-        $rawContent = $request->getContent();
-
-        // Parse the JSON data
-        $data = json_decode($rawContent, true);
-
-        // Log the parsed JSON data
-        Log::info('Webhook JSON Data:', $data);
-
-        // ... Process the JSON data ...
-
-        // Return a response
-       // return response()->json(['message' => 'Webhook received successfully']);
-    } catch (\Exception $e) {
-        Log::error('Error processing webhook:', ['message' => $e->getMessage()]);
-        //return response()->json(['message' => 'Error processing webhook'], 500);
+    {
+        try {
+            // Get the raw content of the request
+            $rawContent = $request->getContent();
+    
+            // Parse the JSON data
+            $data = json_decode($rawContent, true);
+    
+            // Extract and log the common "status" field
+            $status = isset($data['status']) ? $data['status'] : 'Unknown Status';
+            Log::info('Webhook Status:', ['status' => $status]);
+    
+            // Extract and log the "email" field based on the response type
+            if ($status === 'signer-viewed-the-contract' || $status === 'signer-signed') {
+                $email = $data['data']['signer']['email'] ?? 'Unknown Email';
+                Log::info('Signer Email:', ['email' => $email]);
+            } elseif ($status === 'contract-signed') {
+                $signerEmail = $data['data']['contract']['signers'][0]['email'] ?? 'Unknown Signer Email';
+                Log::info('Contract Signed by Signer Email:', ['email' => $signerEmail]);
+            }
+    
+            // ... Process the JSON data ...
+    
+            // Return a response
+             
+        } catch (\Exception $e) {
+            Log::error('Error processing webhook:', ['message' => $e->getMessage()]);
+           // return response()->json(['message' => 'Error processing webhook'], 500);
+        }
     }
-}
-
+    
 
 
     
