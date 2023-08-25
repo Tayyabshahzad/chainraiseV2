e34e83ea57a6f69d399c92fd7d7618a5a2469a88
Tayyab Shahzad Janjua <tayyabshahzad1@outlook.com>
2023-08-25 21:48:49 +0500

    UI Changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,7 +18,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
     integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <title> Chain Rasied Portal | @yield('title') </title>
+    <title> ChainRaise Portal | @yield('title') </title>
 
 
 
