c4fc47d59e3f619e08a45ca599069e52d53df967
Tayyab Shahzad Janjua <tayyabshahzad1@outlook.com>
2023-08-30 03:06:03 +0500

    offer Edit Error Fix

--- app/Http/Controllers/ESignController.php
+++ app/Http/Controllers/ESignController.php
@@ -13,14 +13,14 @@ class ESignController extends Controller
 {
     public function previewDocument(Request $request){
         $request->validate([
-            'user_id' => 'required', 
+            'user_id' => 'required',
             'template_id'=>'required'
         ]);
-         
+
         $token = env('ESIGN_TOKEN');
         $user = User::find($request->user_id);
-        $url = "https://esignatures.io/api/contracts?token=".$token; 
-        try{    
+        $url = "https://esignatures.io/api/contracts?token=".$token;
+        try{
 
             $response = Http::withHeaders([
                 'Content-Type' => 'application/json',
@@ -33,13 +33,13 @@ class ESignController extends Controller
                         "signature_request_delivery_method" => "embedded"
                     ],
                 ]
-            ]); 
-            $json_template = json_decode((string) $response->getBody(), true); 
+            ]);
+            $json_template = json_decode((string) $response->getBody(), true);
            // dd($json_template);
-            if($response->successful()){ 
-                $doc = MyEDocument::where('template_id',$request->template_id)->first(); 
+            if($response->successful()){
+                $doc = MyEDocument::where('template_id',$request->template_id)->first();
                 $doc->status = 'open';
-                $doc->save(); 
+                $doc->save();
                 return response([
                     'status'=>true,
                     'url'=>$json_template['data']['contract']['signers'][0]['sign_page_url']
@@ -47,25 +47,23 @@ class ESignController extends Controller
             }
 
         }catch(Exception $error){
-            if($response->successful()){
-                return response([
-                    'status'=>false,
-                    'url'=>null,
-                    'message'=>$error. " There is some error while getting data"
-                ]);
-            }
+            return response([
+                'status'=>false,
+                'url'=>null,
+                'message'=>$error. " There is some error while getting data"
+            ]);
         }
-          
-        
+
+
     }
 
     public function previewDocumentInvesterFlow($user_id,$template_id){
-      
-         
+
+
         $token = env('ESIGN_TOKEN');
         $e_sign = Http::get('https://esignatures.io/api/templates/' . $template_id . '?token='.$token);
         $json_e_sign = json_decode((string) $e_sign->getBody(), true);
-     
+
         return response([
             'status' => true,
             'data' => $json_e_sign
@@ -74,9 +72,9 @@ class ESignController extends Controller
         $token = env('ESIGN_TOKEN');
         $user = User::find($request->user_id);
 
-        https://esignatures.io/api/contracts/1contr11-2222?token=your-secret-token
-        $url = "https://esignatures.io/api/contracts?token=".$token; 
-        try{    
+        //https://esignatures.io/api/contracts/1contr11-2222?token=your-secret-token
+        $url = "https://esignatures.io/api/contracts?token=".$token;
+        try{
 
             $response = Http::withHeaders([
                 'Content-Type' => 'application/json',
@@ -89,9 +87,9 @@ class ESignController extends Controller
                         "signature_request_delivery_method" => "embedded"
                     ],
                 ]
-            ]); 
-            $json_template = json_decode((string) $response->getBody(), true); 
-       
+            ]);
+            $json_template = json_decode((string) $response->getBody(), true);
+
             if($response->successful()){
                 return response([
                     'status'=>true,
@@ -108,21 +106,21 @@ class ESignController extends Controller
                 ]);
             }
         }
-          
-        
+
+
     }
 
     public function check_esing_status(Request $request){
         $user = Auth::user();
-        $doc = MyEDocument::where('offer_id',$request->offer_id)->where('investor_id',$user->id)->first(); 
+        $doc = MyEDocument::where('offer_id',$request->offer_id)->where('investor_id',$user->id)->first();
         if($doc->status == 'queued'){
             return response([
-                'status'=>false, 
-            ]);    
+                'status'=>false,
+            ]);
         }else{
             return response([
-                'status'=>true, 
-            ]);    
+                'status'=>true,
+            ]);
         }
     }
 }
