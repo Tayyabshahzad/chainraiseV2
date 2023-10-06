a5a406292851232a6fcb2152f8100ebe4350aa92
Tayyab Shahzad Janjua <arhamsaeed294@gmail.com>
2023-10-07 00:32:03 +0500

    UI Chnages

--- resources/views/frontEnd/offer/detail.blade.php
+++ resources/views/frontEnd/offer/detail.blade.php
@@ -208,33 +208,10 @@
                         <h2 class="fs-2 fw-bolder my-5">Company Deck    </h2>
                         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
                             <div class="carousel-indicators">
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="0" class="active" aria-current="true"
-                                    aria-label="Slide 1"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="2" aria-label="Slide 3"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="3" aria-label="Slide 4"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="4" aria-label="Slide 5"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="5" aria-label="Slide 6"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="6" aria-label="Slide 7"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="7" aria-label="Slide 8"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="8" aria-label="Slide 9"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="9" aria-label="Slide 10"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="10" aria-label="Slide 11"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="11" aria-label="Slide 12"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide-to="12" aria-label="Slide 13"></button>
+                                @for($i=0;$i<=$slider_images->count(); $i++)
+                                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="{{ $i }}" class="active" aria-current="true" aria-label="Slide {{ $i }}"></button>
+                                @endfor
+
                             </div>
                             <div class="carousel-inner">
                                 @foreach($slider_images as $slider_image)
